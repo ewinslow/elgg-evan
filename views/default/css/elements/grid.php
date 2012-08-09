287450f4b5567ea27c8e55fcf9576aee9b96f1d0
Evan Winslow <evan@elgg.org>
2012-08-09 14:50:08 -0700

    Adds grids system + removes js dependency for .hidden class

--- views/default/css/elements/grid.php
+++ views/default/css/elements/grid.php
@@ -12,7 +12,7 @@
 $gutterWidthPercent = 1.6; /* 16px on a 1000px grid */
 ?>
 
-.elgg-grid { margin: <?php echo $gutterWidthPercent/2; ?>% 0 <?php echo $gutterWidthPercent; ?>% 0; }
+.elgg-grid + .elgg-grid { margin-top: <?php echo $gutterWidthPercent; ?>%; }
 .elgg-col { float: left; margin-right: <?php echo $gutterWidthPercent; ?>%; }
 .elgg-col-1of1 { float: none; }
 .elgg-col:last-child, .elgg-col-last { float: none; overflow: hidden; margin-right: 0; width: auto; }
@@ -23,7 +23,6 @@ for ($i = 2; $i <= 6; $i++) {
 	$gutters = $i - 1;
 	$columnWidthPercent = (100 - $gutters * $gutterWidthPercent)/$i;
 	for ($j = 1; $j < $i; $j++) {
-		// Want reduced fractions. E.g. no elgg-col-2of6 (since 1of3 covers that)
 		if ($j > 1 && ($i/$j) === (int)($i/$j)) {
 			continue;
 		}
--- views/default/css/evan.php
+++ views/default/css/evan.php
@@ -1,6 +1,6 @@
 /** Some css fixes until they are approved by Elgg */
 
 /** http://trac.elgg.org/ticket/4749 */
-#body .hidden {
+.elgg-page .hidden {
     display: none;   
 }
