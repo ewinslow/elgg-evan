e858816d5337d14ca678e5243147cdbd7e5a910c
Evan Winslow <evan.b.winslow@gmail.com>
2014-10-27 09:28:15 +0000

    fix(di): Temporarily disable loading config from other plugins

--- evan.di.php
+++ evan.di.php
@@ -4,12 +4,16 @@ use Evan\Structs\ArrayCollection;
 
 return array(
 	// TODO(ewinslow): Use classname resolution when we can require PHP 5.5
+	'ElggSite' => DI\factory(function() {
+		return elgg_get_site_entity();
+	}),
 	'Evan\Email\Sender' => DI\object('Evan\Email\ElggSender'),
 	'Evan\I18n\Translator' => DI\object('Evan\I18n\ElggTranslator'),
 	'Evan\Storage\Db' => DI\object('Evan\Storage\MysqlDb'),
 	'Evan\Time\Clock' => DI\object('Evan\Time\SystemClock'),
 	'plugins' => DI\factory(function() {
 		// Register all active plugins for convention-based plugin hooks + events
-		return new ArrayCollection(elgg_get_plugins('active'));
+		// return new ArrayCollection(elgg_get_plugins('active'));
+		return new ArrayCollection();
 	}),
 );
